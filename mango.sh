29834aabb18909bef759fbe1d306113e0342aad8
Nandlal Kamat <38187138+kamatnandlal@users.noreply.github.com>
2025-02-13 18:48:20 +0530

    Update mango.sh

--- mango.sh
+++ mango.sh
@@ -1,9 +1,7 @@
-
-
+#mangooo
+#!/bin/bash
 sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl enable httpd
 sudo systemctl start httpd
-
-
 echo "<h1>Welcome to CentOS AMI</h1>" | sudo tee /var/www/html/index.html
